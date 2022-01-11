3163fa15987bbb0ae319916e96aff84641998d0d
SkyeGee <80256959+SkyeGee@users.noreply.github.com>
2022-01-11 19:10:46 +0200

    update

--- test/EtherCoinToss.js
+++ test/EtherCoinToss.js
@@ -1,8 +1,16 @@
 const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 
-describe("EtherCoinToss", function () {
-   it("Should return the new greeting once it's changed", async function () {
+describe("EtherCoinToss", () => {
+   describe ("getRandomNumber", () => {
+      it("Should return ", async function () {
+
+         describe("functionName", () => {
+               it("should return when given", async () => {
+                   // ...
+           })
+         })
+       })
 //     const Greeter = await ethers.getContractFactory("Greeter");
 //     const greeter = await Greeter.deploy("Hello, world!");
 //     await greeter.deployed();
